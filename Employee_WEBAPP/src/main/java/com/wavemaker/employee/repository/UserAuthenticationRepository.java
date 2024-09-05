f251a385a9355b99f5b9689cba87f6b2742f5b3a
RoopaAenugu <151902380+RoopaAenugu@users.noreply.github.com>
2024-09-05 11:10:41 +0530

    Delete Employee_WEBAPP directory

--- Employee_WEBAPP/src/main/java/com/wavemaker/employee/repository/UserAuthenticationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wavemaker.employee.repository;
-
-import com.wavemaker.employee.model.UserAuthentication;
-
-public interface UserAuthenticationRepository
-{
-
-    public boolean addUserLogin(UserAuthentication userAuthentication);
-}
--- Employee_WEBAPP/src/main/java/com/wavemaker/employee/service/UserAuthenticationService.java
+++
@@ -1,7 +0,0 @@
-package com.wavemaker.employee.service;
-
-import com.wavemaker.employee.model.UserAuthentication;
-
-public interface UserAuthenticationService {
-    public  boolean addUserLogin(UserAuthentication userAuthentication);
-}
